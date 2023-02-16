20da58dc675b8e12be5e90f1cb434bfba663ea78
Felix <felix.schmid@festech.de>
2023-02-16 18:26:23 +0100

    fix

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -47,6 +47,6 @@ long ft_atol(const char *str);
 int ft_isdigit(int arg);
 size_t ft_strlen(const char *str);
 void free_all(t_rules *rules, t_philo **philos);
-void	print_philo(t_philo *philo, char *color, char *state)
+void	print_philo(t_philo *philo, char *color, char *state);
 
 #endif
